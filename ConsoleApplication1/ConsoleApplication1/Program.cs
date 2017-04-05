bf65995c2e4a836d3b6049f8ef71411913a3a1bb
Jan-Pieter Zoutewelle <janpieterzoutewelle@hotmail.com>
2017-04-05 14:47:49 +0200

    Working endpoint

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using NServiceBus;
+using NServiceBus.Features;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Run().GetAwaiter().GetResult();
+        }
+
+        public async Task Run()
+        {
+            var bus = await SetupBus().ConfigureAwait(false);
+            Console.ReadLine();
+        }
+
+        private async Task<IEndpointInstance> SetupBus()
+        {
+            EndpointConfiguration configuration = new EndpointConfiguration("DevelopmentConsole");
+            var transport = configuration.UseTransport<MsmqTransport>();
+            var scanner = configuration.AssemblyScanner();
+            scanner.ScanAppDomainAssemblies = true;
+            configuration.SendOnly();
+            configuration.DisableFeature<Audit>();
+            configuration.UsePersistence<InMemoryPersistence>();
+            var endpoint = await Endpoint.Create(configuration).ConfigureAwait(false);
+            return await endpoint.Start().ConfigureAwait(false);
+        }
+    }
+}
